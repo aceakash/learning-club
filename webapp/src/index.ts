9a839954ea170587b9b3954ecc378d21cb7a2aa6
Akash K <akashkurdekar@gmail.com>
2025-09-07 17:57:26 +0100

    use PORT env var

--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -4,6 +4,7 @@ import ReactDOMServer from "react-dom/server";
 import Home from "./components/Home";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   const html = ReactDOMServer.renderToString(React.createElement(Home));
@@ -14,6 +15,6 @@ app.get("/repositories", (req: Request, res: Response) => {
   res.send("List of GitHub repositories");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
